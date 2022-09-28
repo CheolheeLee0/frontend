547e7e7223be96d50b2ac96d326d55cf57e47785
CheolheeLee0 <wks0968@gmail.com>
2022-09-28 20:59:57 +0900

    Add AWS TEST text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Test End AWS TEST
         </a>
       </header>
     </div>
